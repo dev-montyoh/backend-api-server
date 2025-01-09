521c45f08dc01ee41f43322daf5ab828197cfaef
Monty Oh <ohhanjin0330@gmail.com>
2025-01-09 10:34:20 +0900

    [FIX] Develop 배포 추가 중  - Dockerhub 배포 테스트

--- .github/docker/package.sh
+++ .github/docker/package.sh
@@ -8,8 +8,8 @@ VERSION=$3
 ##  Docker 이미지 빌드
 echo "Building Docker image..."
 docker build -f Dockerfile  \
--t "$SERVICE_NAME":"$VERSION" \
--t  "$SERVICE_NAME":latest  \
+-t monty082/backend-api-server:"$VERSION" \
+-t monty082/backend-api-server:latest  \
 --build-arg SERVICE_NAME="$SERVICE_NAME"  \
 --build-arg PROFILE="$PROFILE"  \
 --build-arg VERSION="$VERSION"  \
